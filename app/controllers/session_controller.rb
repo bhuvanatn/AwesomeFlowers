b14fbe06dea07dabe712e04623417886fb9139f9
Bhuvana <bhuvanatn@gmail.com>
2016-05-05 20:59:33 +1000

    login/signup

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,17 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+
+  def create
+   user = User.find_by :email => params[:email]
+     if user.present? && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       #log em in
+       redirect_to root_path
+     else
+       flash[:error] = "Oops...try again."
+       redirect_to login_path
+   end
+  end
+end
